9f7f518d4790566e5ff9200080141c42cf4027c7
Weber <weber.chu@prophetdigits.com>
2024-06-15 14:16:06 +0800

    Initial Commit

---
+++ CustomTab/CustomTabApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTabApp.swift
+//  CustomTab
+//
+//  Created by Wei Chu on 2024/6/15.
+//
+
+import SwiftUI
+
+@main
+struct CustomTabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
